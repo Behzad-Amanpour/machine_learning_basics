de7d5ae627271be0d39581c468740a1e60b2442b
Behzad-Amanpour <116773990+Behzad-Amanpour@users.noreply.github.com>
2023-06-05 19:48:55 +0330

    Update code.py

--- code.py
+++ code.py
@@ -17,8 +17,9 @@ model = SVC(kernel='linear')
 # Training -----------------------------------------------------------------------
 model.fit(X_train, y_train)
 
-# Test ---------------------------------------------------------------------------
-print(model.score(X_test, y_test))
+# Test & Prediction --------------------------------------------------------------
+print(model.score(X_test, y_test))  # accuracy on the test data
+y_pred = model.predict(X_test)      # prediction for the test data
 
 # Cross-validation (method1) -----------------------------------------------------
 from sklearn.model_selection import cross_val_score
@@ -40,6 +41,65 @@ for train_index, test_index in kf.split(X):
     scores.append(model.score(X_test, y_test))
 print(scores)
 print(np.mean(scores))
+
+# Evaluation Metrics ------------------------------------------------------------
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, precision_recall_fscore_support
+from sklearn.metrics import balanced_accuracy_score
+def specificity_score(y_true, y_pred):  # Scikit-learn has not defined a function for specificity
+    p, r, f, s = precision_recall_fscore_support(y_true, y_pred)
+    return r[0]
+
+#  Each function takes two 1-dimensional numpy arrays: the true values of the target & the predicted values of the target.
+print("accuracy:", accuracy_score(y_test,y_pred))
+print("precision:", precision_score(y_test,y_pred))
+print("recall (sensitivity):", recall_score(y_test,y_pred))
+print("specificity:", specificity_score(y_test,y_pred))
+
+print("balanced accuracy:", balanced_accuracy_score(y_test,y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
