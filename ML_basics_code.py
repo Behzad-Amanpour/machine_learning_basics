be89b066b041f46884f3143efcbb29d2fe10607c
Behzad-Amanpour <116773990+Behzad-Amanpour@users.noreply.github.com>
2023-06-18 00:35:25 +0330

    Update ML_basics_code.py

--- ML_basics_code.py
+++ ML_basics_code.py
@@ -42,27 +42,6 @@ for train_index, test_index in kf.split(X):
 print(scores)
 print(np.mean(scores))
 
-# Evaluation Metrics ------------------ Behzad Amanpour --------------------------
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, precision_recall_fscore_support
-from sklearn.metrics import balanced_accuracy_score
-def specificity_score(y_true, y_pred):  # Scikit-learn has not defined a function for specificity
-    p, r, f, s = precision_recall_fscore_support(y_true, y_pred)
-    return r[0]
-
-print("accuracy:", accuracy_score(y_test,y_pred))
-print("precision:", precision_score(y_test,y_pred))
-print("recall (sensitivity):", recall_score(y_test,y_pred))
-print("specificity:", specificity_score(y_test,y_pred))
-print("balanced accuracy:", balanced_accuracy_score(y_test,y_pred))
-
-# Confusion Matrix ---------------------------------------------------------------
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
-
-labels = ['negative', 'positive']
-cm = confusion_matrix( y_test, y_pred )
-ConfusionMatrixDisplay( cm, display_labels = labels ).plot()
-
 # ROC Curve & AUC ----------------------- Behzad Amanpour ------------------------
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, roc_auc_score
